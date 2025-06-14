0c3626db365756e71542a0ced9847c29812e223c
miguelleite21 <miguelleite360@gmail.com>
2025-06-14 01:52:22 -0300

    feat: Created url base structure

--- src/urls/entities/url.entity.ts
+++ src/urls/entities/url.entity.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryKey, Property, ManyToOne } from '@mikro-orm/core';
-import { User } from '../user/user.entity';
+import { User } from '../../user/user.entity';
 
 @Entity()
 export class ShortUrl {
@@ -19,7 +19,7 @@ export class ShortUrl {
   clickCount: number = 0;
 
   @Property({ onCreate: () => new Date() })
-  createdAt: Date = new Date();
+  createdAt?: Date = new Date();
 
   @Property({ onUpdate: () => new Date(), nullable: true })
   updatedAt?: Date;
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,5 +1,4 @@
 import { Entity, PrimaryKey, Property, Unique } from '@mikro-orm/core';
-import * as bcrypt from 'bcrypt';
 
 @Entity()
 @Unique({ properties: ['email'] })
@@ -14,19 +13,11 @@ export class User {
   passwordHash!: string;
 
   @Property({ onCreate: () => new Date() })
-  createdAt: Date = new Date();
+  createdAt?: Date = new Date();
 
   @Property({ onUpdate: () => new Date(), nullable: true })
   updatedAt?: Date;
 
   @Property({ nullable: true })
   deletedAt?: Date;
-
-  async setPassword(password: string): Promise<void> {
-    this.passwordHash = await bcrypt.hash(password, 10);
-  }
-
-  async comparePassword(password: string) {
-    return bcrypt.compare(password, this.passwordHash);
-  }
 }
